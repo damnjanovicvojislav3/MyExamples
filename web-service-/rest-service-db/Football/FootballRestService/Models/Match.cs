c9e76cae908273e41d1918dc930ad0143a1e7953
damnjanovicvojislav3 <damnjanovicvojislav3@gmail.com>
2026-01-07 16:00:29 +0100

    match ch

--- web-service-/rest-service-db/Football/FootballRestService/Models/Match.cs
+++ web-service-/rest-service-db/Football/FootballRestService/Models/Match.cs
@@ -10,16 +10,17 @@ namespace FootballRestService.Models
         [Key]
         [Column("match_id")]
         public int MatchId { get; set; }
-       
 
         [Column("home_team_id")]
         public int HomeTeamId { get; set; }
 
+        [ForeignKey(nameof(HomeTeamId))]
         public virtual Team? HomeTeam { get; set; }
 
         [Column("away_team_id")]
         public int AwayTeamId { get; set; }
 
+        [ForeignKey(nameof(AwayTeamId))]
         public virtual Team? AwayTeam { get; set; }
 
         [Column("match_date", TypeName = "date")]
